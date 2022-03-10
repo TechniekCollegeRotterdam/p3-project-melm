c6eefec38a99c20d1f2c2819cae19524a4e363a8
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-03-10 13:59:10 +0100

    .

--- infcatprodprijs.php
+++ infcatprodprijs.php
@@ -15,6 +15,7 @@
 require_once("dbconnect.php");
 
 $query = $db-> prepare("SELECT idpurchase, pricecharged FROM `purchaseline` WHERE pricecharged > '100.00';");
+SELECT idtype, name, price FROM `type` INNER JOIN `product` ON type.idtype = product.idtype WHERE price > '100';
 $query->execute();
 $resultq = $query->fetchALL(PDO::FETCH_ASSOC);
 foreach ($resultq as $data){
