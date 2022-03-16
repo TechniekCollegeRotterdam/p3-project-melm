9e0c2ce88cf1427559e555a8efd050cb2d39c81c
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-03-16 16:24:14 +0100

    form

---
+++ registratie.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Document</title>
+</head>
+
+<body>
+       <header>
+
+       </header>
+       <main>
+
+
+              <h1 class="Registratie">Registratie</h1>
+
+
+              <form class="registratie" id="registratie">
+
+                     <section class="registratie-section">
+                            <label name="id">Id klant *</label>
+                            <input type="text" placeholder="John">
+                            <br>
+                            <label name="name"> Achternaam *</label>
+                            <input type="text" placeholder="Doe">
+
+                            <br>
+                            <label name="naam">Voornaam *</label>
+                            <input type="text" placeholder="Jack">
+
+                            <br>
+                            <label name="naam">Tweede naam *</label>
+                            <input type="text" placeholder="Tom">
+
+                            <br>
+                            <label name="titel">Titel *</label>
+                            <br>
+                            <label name="titel">Mr *</label>
+                            <input type="radio" placeholder="mr">
+                            <br>
+                            <label name="titel">Mrs *</label>                            
+                            <input type="radio" placeholder="mrs">
+                            <br>
+                            <label name="titel">Ms *</label>
+                            <input type="radio" placeholder="ms">
+
+                            <br>
+                            <label name="Geslacht">Geslacht *</label>
+                            <br>
+                            <label name="gender">Man </label>
+                            <input type="radio" placeholder="geslacht">
+                            <br>
+                            <label name="gender">Vrouw </label>
+                            <input type="radio" placeholder="geslacht">
+
+                            <br>
+                            <label name="straatnaam">Straatnaam *</label>
+                            <input type="text" placeholder="Hooghweg">
+
+                            <br>
+                            <label name="stad">Stad *</label>
+                            <input type="text" placeholder="Rotterdam">
+
+                            <br>
+                            <label name="postcode">Postcode *</label>
+                            <input type="text" placeholder="3030AA">
+
+                            <br>
+                            <label name="landnummer">Land nummer *</label>
+                            <input type="text" placeholder="157">
+
+                            <br>
+                            <label name="email">Email *</label>
+                            <input type="mail" placeholder="email@gmail.com">
+
+                            <br>
+                            <label name="tel">Telefoonnummer *</label>
+                            <input type="tel" placeholder="06 99 88 77 66">
+
+                            <br>
+                            <label name="birhtday">Geboortedatum *</label>
+                            <input type="date" placeholder="date">
+
+                            <br>
+                            <label name="beroep">Beroep *</label>
+                            <input type="text" placeholder="beroep">
+
+
+
+                     </section>
+
+                     <section class="submit-reset">
+                            <input type="submit" name="submit" value="submit">
+                            <input type="reset" name="reset" value="reset">
+                     </section>
+
+
+
+
+
+
+
+
+
+
+
+       </main>
+       <footer>
+
+       </footer>
+</body>
+
+</html>
