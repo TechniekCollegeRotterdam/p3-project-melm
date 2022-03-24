01c3d6efe2d87b46c553f6ddf351131f92f5233a
Maik Alexander Lüdeck <9016083@student.tcrmbo.nl>
2022-03-24 20:30:25 +0100

    done

--- webrv.php
+++ webrv.php
@@ -34,32 +34,27 @@
                     </li>
                     <br>
                     <li>
-                        Konden Jullie alles op website wat toegestaan is vinden en naar toe gaan als gast/gerigstreerde klant
-                        <input type="radio" name="what" value="werknemer">ja 
-                        <input type="radio" name="what" value="Klant"> nee
+                    Werkten alle pagina's en functies op website Ja of nee? ?
+                        <input type="radio" name="what" value="ja">ja 
+                        <input type="radio" name="what" value="nee"> nee
                     </li>
                     <br>
                     <li>
-                        <label for="range">van 1 tot 10 hoe goed werkend was de website
+                    Als nee hoe? <textarea name="comment" rows="5" cols="40"></textarea>
+                     </li>
+                     <br>
+                    <li>
+                        <label for="range">van 1 tot 10 hoe goed werkend is de website?
                         </label>
                         <input id="range" type="range" name="range" Value="0" min="1" max="10" step="1">
                     </li>
 				<li>
-                    
                    opmerking over website <textarea name="comment" rows="5" cols="40"></textarea>
                     </li>
 </ul>
                 <input type="Reset" value="Reset">
                 <input type="submit" value="Versturen">
             </form>
-        <!-- Voornaam achternaam Email
-		Vragen die geruikt worden :
-		Werkten alle pagina's en functies op website Ja of nee? 
-		Als nee welke pagina's en hoe werkte dat niet. 
-		Was de website prettig voor het oog aka hoe was de uiterlijk van de web Ja, nee, gedeeltelijk ?
-		Als nee of gedeeltelijk leg uit waar en waarom en hoe we het waarschijnlijk kunnen verbeteren. 
-		als je de website sterren kon geven van 1 tot 5, hoeveel sterren zou deze web waard zijn?
-		Verzenden -->
     </main>
 </body>
 </html>
