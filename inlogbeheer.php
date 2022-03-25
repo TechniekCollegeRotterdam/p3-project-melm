70698c4d15162350bd9f10b5b5b1985db468d978
Maik Alexander Lüdeck <9016083@student.tcrmbo.nl>
2022-03-25 11:50:49 +0100

    ...

--- inlogbeheer.php
+++ inlogbeheer.php
@@ -4,7 +4,7 @@
     <head>
     	<meta charset="UTF-8">
     	<title> Inloggen Beheerder </title>
-    	<link rel="stylesheet" type="text/css" href="company.css">
+    	<link rel="stylesheet" type="text/css" href="company.scss">
     </head>
     <body>
     	<header>
