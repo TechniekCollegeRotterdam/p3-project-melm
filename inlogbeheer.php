18cbc49b70a41244ec660dd278a8686403957933
Maik Alexander Lüdeck <9016083@student.tcrmbo.nl>
2022-03-30 09:18:51 +0200

    ZA WARUDO (i do not what i am doing with my life)

--- inlogbeheer.php
+++ inlogbeheer.php
@@ -18,58 +18,58 @@ include "nav.html";
     	<main>
 		<?php
 require_once 'dbconnmelm.php';
-if (isset($_POST['login'])) {
-	try {
-		$sQuery = "SELECT * FROM client WHERE idclient = :idclient";
-		$oStmt = $db -> prepare($sQuery);
-		$oStmt -> bindValue(parameter: ':idclient', $_POST['password']);
-		$oStmt -> execute();
-		if ($oStmt -> rowCount() == 1) {
-			$rij = $oStmt -> fetch(fetch_style: PDO::FETCH_ASSOC);
-			if (password_verify($_POST['password'], $rij['password'])) {
-				$_SESSION['idclient'] = $rij['idclient'];
-				$_SESSION['givenname'] = $rij['givenname'];
-				$_SESSION['surname'] = $rij['surname'];
-				if ($rij['beheer'] == "J") {
-					$_SESSION['blogin'] = true;
-					header(string: 'Refresh: 3; url=beheerhome.php');
-					echo "login succesvol";
-				} 
-				    //else {
-					//$_SESSION['klogin'] = true;
-					//header(string: 'Refresh:3; url=index.php');
-					//echo "You made me proud person who i wil never meet. login succsesvol";}
+if (isset($_POST["loginAdmin"])){
+  try {
+			$sQuery = "SELECT 'idclient', 'surname', 'givename', 'emailadress','passwrd', 'admin' FROM client WHERE admin='yes' ";
+			$oStmt = $db -> prepare($sQuery);
+			$oStmt -> bindValue(parameter: ':idclient', $_POST['passwrd']);
+			$oStmt -> execute();
+			if ($oStmt -> rowCount() == 1) {
+				$rij = $oStmt -> fetch(fetch_style: PDO::FETCH_ASSOC);
+				if (password_verify($_POST['passwrd'], $rij['passwrd'])) {
+					$_SESSION['idclient'] = $rij['idclient'];
+					$_SESSION['givenname'] = $rij['givenname'];
+					$_SESSION['surname'] = $rij['surname'];
+					if ($rij['admin'] == "yes") {
+						$_SESSION['blogin'] = true;
+						header(string: 'Refresh: 3; url=beheerhome.php');
+						echo "login succesvol, welcome highranker";
+					} 
+						//else {
+						//$_SESSION['klogin'] = true;
+						//header(string: 'Refresh:3; url=index.php');
+						//echo "You made me proud person who i wil never meet. login succsesvol";}
+					}
+				} else {
+					header(string: 'refresh: 4; url=inlogbeheer.php');
+	
+	
+	
+	
+					echo "<div class='container'>";
+					echo "<div class='panel panel-primary'>";
+					echo "<div class='panel-heading'>You disappointed this entire universe! Redeem yourself.</div>";
+					echo "<div class='panel-body'>YOU FREAKING DONKY! You had one job and ya blew it! Start from schratch.</div>";
+					
+						echo "</div>";
+						echo "</div>"; 
 				}
-			} else {
-				header(string: 'refresh: 4; url=inlogbeheer.php');
-
-
-
-
-				echo "<div class='container'>";
-				echo "<div class='panel panel-primary'>";
-				echo "<div class='panel-heading'>You disapointed this entire universe! Redeem yourself.</div>";
-				echo "<div class='panel-body'>YOU FREAKING DONKY! You had one job and ya blew it! Start from schratch.</div>";
-				
-					echo "</div>";
-	                echo "</div>"; 
 			}
 		}
-   }
-?>	
-<form>;
-	<method post>
-	<label for = 'emailaddress'>Email</label>;
-				<input type='email' name='emailAddress' class='form-control' placeholder='Email' required>;
-
-				<label for = 'password'>Wachtwoord</label>;
-				<input type = 'password' name='password' class='form-control' placeholder='Wachtwoord' required>";
-
-				</form>;
-				<br>;
-				<form>;
-				<button class='#' type='submit' name='button'>Log in</button>;
-</form>;
+	?>	
+	<form>;
+		<method post>
+		<label for = 'emailaddress'>Email</label>;
+					<input type='email' name='emailAddress' class='form-control' placeholder='Email' required>;
+	
+					<label for = 'password'>Wachtwoord</label>;
+					<input type = 'password' name='password' class='form-control' placeholder='Wachtwoord' required>";
+	
+					</form>;
+					<br>;
+					<form>;
+					<button class='btn' type='submit' name='button'>Log in</button>;
+	</form>;
 </main>
 </body>
 </html>
