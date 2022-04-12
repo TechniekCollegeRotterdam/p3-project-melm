b2a4f5ee00aad92a601c2d8e28ec13527ff38b0c
9007170 <9007170@student.zadkine.nl>
2022-04-12 10:08:00 +0200

    inlog klant

--- inlogklant.php
+++ inlogklant.php
@@ -16,7 +16,7 @@ include "nav.html";
 <body>
 <header>
         <h1>melm</h1>
-        <!--  menu opgehaald. -->
+        <!--  menu opgehalen. -->
 
     </header>
   <main>
@@ -48,56 +48,5 @@ include "nav.html";
 
 
 </body>
-inlogproces klant
-<?php
-session_start();
-    if(isset ($_POST['submitBtnLogin'])) {
-    require 'dbconnect.php';
-    $userId = $_POST['userid'];
-    $pword = $_POST['password'];
-    try {
-        $query = "SELECT * FROM client WHERE emailadress = :email";
-        $stmt = $db->prepare($query);
-        $stmt->bindValue(':email', $userId);
-        $stmt->execute();
-        if ($stmt->rowCount() == 1) {
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if(password_verify($pword, $result['passwrd']))
-            {
-                $_SESSION['clogin'] = true;
-                $_SESSION['idclient'] = $result['idclient'];
-                $_SESSION['surname'] = $result['surname'];
-                $_SESSION['givenname'] = $result['givenname'];
-                echo "<p>";
-                echo "U bent succesvol ingelogd!";
-                echo "</p>";
-                header('Refresh: 3; url=homeklant.php');
 
-
-            } else {
-                echo "<p>";
-                echo "Combinatie klopt niet!";
-                echo "</p>";
-                header('Refresh: 3; url=index.php');
-    exit(); 
-            }
-        } else {
-            echo "<p>";
-            echo "Combinatie klopt niet!";
-            echo "</p>";
-            header('Refresh: 3; url=index.php');
-    exit(); 
-        }
-    } catch(PDOException $e)
-    {
-    $sMsg = '<p>
-    Regelnummer: '.$e->getLine().'<br> />
-    Bestand: '.$e->getFile().'<br> />
-    Foutmelding: '.$e->getMessage().'
-    </p>';
-    trigger_error($sMsg);
-    }
-    }
-
-?>
 
