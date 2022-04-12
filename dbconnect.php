9385a2b5b3dd09db45d98037fcb9bfdabf0033ce
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-04-12 11:21:08 +0200

    klaar

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 try 
 { 
@@ -15,36 +14,3 @@ catch(PDOException $e)
     trigger_error($sMsg); 
 } 
 ?> 
-=======
-
-<?php
-
-try
-
-{
-
-    $db = new PDO('mysql:host=localhost;dbname=melm', 'root' ,'');
-
-}
-
-catch(PDOException $e)
-
-{
-
-    $sMsg = '<p>
-
-            Regelnummer: '.$e->getLine().'<br />
-
-            Bestand: '.$e->getFile().'<br />
-
-            Foutmelding: '.$e->getMessage().'
-
-        </p>';
-
-
-    trigger_error($sMsg);
-
-}
-
-?>
->>>>>>> 59dfd36a3f9443a7b23cfa497e90690bad693366
