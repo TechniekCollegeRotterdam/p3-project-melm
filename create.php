2673d4e95df3a6f8a2c6ac2e47c295a4d9c4c557
Maik Alexander Lüdeck <9016083@student.tcrmbo.nl>
2022-04-20 11:49:31 +0200

    insertdone

--- create.php
+++ create.php
@@ -1,8 +1,24 @@
 <?php
 require("dbconnmelm.php");
 
+
+if($_POST)
+{
+        require_once "dbconnmelm.php";
+        $typeid = filter_var($_POST["typeid"], FILTER_SANITIZE_STRING);
+        $query = $db->prepare("SELECT * FROM types WHERE idtype = :typeid");
+        $query->bindValue(':typeid', $typeid);
+        $query->execute();
+        if($query->rowCount() <> 1 )
+        {
+               $_POST = False;
+               echo "Geen matchend id.";
+               
+       }
+}
+
 if($_POST) {
-    $idproduct = trim($_POST['idproduct']);
+    //$idproduct = trim($_POST['idproduct']);
     $typeid = (int)$_POST['typeid'];
     $stockquantity = (int)$_POST['stockquantity'];
     $price = (float) $_POST['price'];
@@ -13,12 +29,31 @@ if($_POST) {
 
 
     try{
-        $sql = 'INSERT INTO product(idproduct, typeid, stockquantity, price, imageref, prodname, proddesc, origincountry)
-                VALUES(:idproduct, :typeid, :stockquantity, :price, :imageref, :prodname, :proddesc, :origincountry)';
+        $sql = 'INSERT INTO product(typeid, stockquantity, price, imageref, prodname, proddesc, origincountry)
+                VALUES(:typeid, :stockquantity, :price, :imageref, :prodname, :proddesc, :origincountry)';
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(":idproduct", $idproduct);
-
-    } catch (Exception)
+        $stmt = $db->prepare($sql);
+        //$stmt->bindParam(":idproduct", $idproduct);
+        $stmt->bindParam(":typeid", $typeid);
+        $stmt->bindParam(":stockquantity", $stockquantity);
+        $stmt->bindParam(":price", $price);
+        $stmt->bindParam(":imageref", $imageref);
+        $stmt->bindParam(":prodname", $prodname);
+        $stmt->bindParam(":proddesc", $proddesc);
+        $stmt->bindParam(":origincountry", $origincountry);
+        $stmt->execute();
+        if($stmt->rowCount()) {
+            header("location: add.php?status=created");
+            exit();
+        }
+        header( "location: add.php?status=fail_create");
+        exit();
+    } catch (Exception $e) {
+       echo "error ". $e->getMessage();
+       exit();
+    }
+} else {
+    header("location: add.php?status=fail_create");
+    exit();
 }
 ?>
